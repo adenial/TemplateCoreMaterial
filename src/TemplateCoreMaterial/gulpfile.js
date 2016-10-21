690c7c8b02384dd467d71547db69b98f26bb4ce5
adenial <adenialster@gmail.com>
2016-10-21 00:55:21 -0500

    removed mdl source code, only left material.css and material.js, added gulp config file and sass task

---
+++ src/TemplateCoreMaterial/gulpfile.js
@@ -0,0 +1,21 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+// does not work
+// var project = require('./project.json');
+var destPath = "wwwroot/";
+
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  fs = require("fs"),
+  sass = require("gulp-sass");
+
+gulp.task("sass", function ()
+{
+  return gulp.src('Styles/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(destPath + '/css'));
+});
